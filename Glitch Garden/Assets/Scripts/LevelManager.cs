c4f0e9ce3fa0d1b19d1bcde8c410c09a90ae3645
otyx <richard.andrews@otyx.com>
2016-10-10 21:02:31 +1000

    Complete Lesson 132
    Standardise menu pages and remove LM as DoNotDestroyOnLoad

--- Glitch Garden/Assets/Scripts/LevelManager.cs
+++ Glitch Garden/Assets/Scripts/LevelManager.cs
@@ -6,13 +6,21 @@ public class LevelManager : MonoBehaviour {
 
 	public float autoLoadLevelInterval;
 
-	void Awake() {
-		DontDestroyOnLoad (gameObject);
-	}
+	// No need to keep between levels since only using the prefab for level changes.
+	// Also, by default, autoloadlevel is 0 so we don't get automatic level loading
+	// Therefore, to move from splash to start,change the value to 3 seconds (for example)
+	//
+	//void Awake() {
+	//	DontDestroyOnLoad (gameObject);
+	//}
 
 	// Use this for initialization
 	void Start () {
-		Invoke ("LoadStartScene", autoLoadLevelInterval);
+		if (autoLoadLevelInterval == 0) {
+			Debug.Log ("LM: Level Auto Load - disabled");
+		} else {
+			Invoke ("LoadStartScene", autoLoadLevelInterval);
+		}
 	}
 
 	public void LoadStartScene() {
