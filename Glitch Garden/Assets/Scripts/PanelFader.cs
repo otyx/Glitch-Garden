9f88ddf166adc9b720201dccc4e7328dcff66a15
otyx <richard.andrews@otyx.com>
2016-10-11 21:18:08 +1000

    Complete Lesson 133
    Add fader panel.

---
+++ Glitch Garden/Assets/Scripts/PanelFader.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PanelFader : MonoBehaviour {
+
+	// the time for the fade in effect
+	public float fadeInTime;
+
+	private Image fadePanelImage;
+	private Color currentColor = Color.black;
+
+	void Awake() {
+		fadePanelImage = gameObject.GetComponent<Image> ();
+		fadePanelImage.color = currentColor;
+	}
+
+	// Use this for initialization
+	void Start () {
+		fadePanelImage.CrossFadeAlpha (0f, fadeInTime, false);
+	}
+	
+	void Update() {
+		if (Time.timeSinceLevelLoad >= fadeInTime) {
+			fadePanelImage.enabled = false;
+		}
+	}
+}
