7646c78f53dfd88a90a313a7d3d4a243e628efbe
otyx <richard.andrews@otyx.com>
2016-10-24 06:59:14 +1000

    Start implementing collisions on attackers

---
+++ Glitch Garden/Assets/Scripts/Attacker.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attacker : MonoBehaviour {
+
+	[Range (-1f, 1.5f)]
+	public float walkSpeed;
+
+	// Use this for initialization
+	void Start () {
+		Rigidbody2D myRigidBody = gameObject.AddComponent<Rigidbody2D> ();
+		myRigidBody.isKinematic = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.left * walkSpeed * Time.deltaTime);
+	}
+}
